d08c4ec9a8aaa473d5a782f1a2e54c48ef33c2c7
Jennifer Hu <jhu890@gmail.com>
2022-12-14 11:40:46 -0500

    Tweak scripts

--- query_hf.sh
+++ query_hf.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# NOTE: it's not recommended to run this as a for-loop. Run in parallel instead.
-
 # Main analyses
 for model in gpt2 google/flan-t5-base google/flan-t5-xl allenai/tk-instruct-3b-def-pos allenai/tk-instruct-11b-def-pos; do
-    python query_hf.py -P $phen -M $model -S $seed
+    # all phenomena and seeds will be run by default
+    python query_hf.py -M $model 
 done
 
 # No-story analyses
--- query_openai.sh
+++ query_openai.sh
@@ -10,10 +10,9 @@ for seed in {0..4}; do
 done
 
 # No-story analyses
+model="text-davinci-002"
 for seed in {0..4}; do
     for phen in Deceits Irony IndirectSpeech Maxims Metaphor; do  
-        for model in text-davinci-002; do 
-            python query_openai.py -P $phen -M $model -S $seed --num_examples 0 --suffix no-story
-        done
+        python query_openai.py -P $phen -M $model -S $seed --num_examples 0 --suffix no-story
     done
 done
